1060e70b597987204b0b62389350a34a223be98e
blakemiller99 <blake@mitostudios.com>
2021-05-27 05:09:22 -0700

    Create members-loop.php

---
+++ members-loop.php
@@ -0,0 +1,11 @@
+
+
+									<div class="member-custom-field-company-directory">
+										<?php echo xprofile_get_field_data( 'Company', bp_get_member_user_id() ); ?>	
+									</div>	
+                            
+                                    <div class="member-custom-field-title-directory">
+										<?php echo xprofile_get_field_data( 'Title', bp_get_member_user_id() ) ; ?>
+									</div>	
+
+
